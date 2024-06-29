91ffc787adde1e5d4d0f47e69fa6ad96dacc970a
Mr-peipei <wushumenmenmen2@gmail.com>
2024-06-29 10:52:45 +0900

    hot-reload有効化

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	  // NOTE: Windowsのdevcontainerでhot-reloadを有効化する。
+    webpack: (config, context) => {
+        config.watchOptions = {
+            ignored: /node_modules/,
+            poll: 1000,
+            aggregateTimeout: 300
+        }
+        return config
+    }
+};
 
 export default nextConfig;
